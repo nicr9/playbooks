ab35c67211a64b4c9192b450aa92a8cd13dd2375
Nic Roland <nicroland9@gmail.com>
2020-07-13 03:13:35 +0000

    feat(ssh): create ssh keypair, upload to github

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Install prerequisites
-sudo apt install -y xclip python3-pip
+sudo apt install -y python3-pip
 sudo pip3 install -r requirements.txt
 
 # Install .extras file and fill it out before executing it
@@ -9,12 +9,4 @@ cp ./dotfiles/dotextras.template $HOME/.extras
 vim $HOME/.extras
 source $HOME/.extras
 
-# Create ssh key pair, add to authorized hosts, add github hostkey
-ssh-keygen -t rsa -b 4096 -C $EMAIL
-ssh-copy-id $USER@$(hostname)
 ssh -T -oStrictHostKeyChecking=no git@github.com
-
-# Put public key into clip board so that it can be easily added to github account
-xclip -sel clip < ~/.ssh/id_rsa.pub
-
-echo "Your new key is saved to the clipboard, add it to your github account here: https://github.com/settings/ssh"
