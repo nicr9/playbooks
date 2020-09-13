003ec51ea570d19d778c6152f165b9bc64869742
Nic Roland <nicroland9@gmail.com>
2020-09-13 15:21:14 +0000

    refactor(bashrc): setting env vars in system playbook

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,4 @@
 sudo apt install -y python3-pip
 sudo pip3 install -r requirements.txt
 
-# Install .extras file and fill it out before executing it
-cp ./dotfiles/dotextras.template $HOME/.extras
-vim $HOME/.extras
-source $HOME/.extras
-
 ssh -T -oStrictHostKeyChecking=no git@github.com
